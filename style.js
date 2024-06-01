358fdc87ee132487060fb31ed80f21f79b4ebd9b
abhi111456 <abhishek.21jics039@jietjodhpur.ac.in>
2024-06-01 15:02:29 +0530

    a

---
+++ style.js
@@ -0,0 +1,8 @@
+const dayInput = document.getElementById("day"),
+ monthInput = document.getElementById("month"), 
+ yearInput = document.getElementById("year"), 
+ daydd = document.getElementById("DD"),
+monthmm = document.getElementById("MM"),
+yearyy = document.getElementById("YY"), 
+form = document.querySelector("form");
+
